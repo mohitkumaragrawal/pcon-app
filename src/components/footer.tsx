58c0c2bddd4f3b60e063c7d2b1ab2b97ee7f0382
Mohit Kumar Agrawal <mhtkrag@gmail.com>
2024-02-21 20:29:20 +0530

    nice footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -10,11 +10,12 @@ export default function Footer() {
   return (
     <HomeSection
       className={cn(
-        "rounded-none mx-0 mt-20 bg-cyan-500/[0.1] flex flex-col items-center"
+        "rounded-none mx-0 mt-20 bg-cyan-500/[0.1] flex flex-col lg:flex-row justify-between items-center",
+        "py-8 gap-5"
       )}
     >
-      <Image src={logo} alt="PCon Logo" height={100} />
-      <p className="mt-6 text-2xl text-center">
+      <Image src={logo} alt="PCon Logo" height={64} />
+      <p className="mt-2 text-2xl text-center font-semibold">
         Programming Club of NIT Jamshedpur
       </p>
       <div className="flex space-x-8 mt-4">
