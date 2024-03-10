25514915b4166bbdd1c508db3a328dd030149171
Mohit Kumar Agrawal <mhtkrag@gmail.com>
2024-03-10 22:42:54 +0530

    Added favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,10 +11,35 @@ import Footer from "@/components/footer";
 
 import { Analytics } from "@vercel/analytics/react";
 
+const description =
+  "We, at Programming Club of NIT Jamshedpur, are a group of highly enthusiastic and dedicated individuals striving to spread algorithmic thinking to ignite the minds of the contemporary generation to Code for the Future. PCON consists of a diverse group which focuses on Competitive Coding, App and Web development, Cloud Computing, Machine Learning, Blockchain and many more. If you think that the bits are the alphabets of the future, you are One of Us, spreading the gene-o-code all the way!";
+
 export const metadata: Metadata = {
   title: "PCON: Programming Club Of NIT Jamshedpur",
-  description:
-    "We, at Programming Club of NIT Jamshedpur, are a group of highly enthusiastic and dedicated individuals striving to spread algorithmic thinking to ignite the minds of the contemporary generation to Code for the Future. PCON consists of a diverse group which focuses on Competitive Coding, App and Web development, Cloud Computing, Machine Learning, Blockchain and many more. If you think that the bits are the alphabets of the future, you are One of Us, spreading the gene-o-code all the way!",
+  description,
+  robots: {
+    index: true,
+    follow: true,
+  },
+
+  openGraph: {
+    type: "website",
+    locale: "en-US",
+    url: "https://pconjsr.in",
+    title: "PCON: Programming Club Of NIT Jamshedpur",
+    description,
+    images: [
+      {
+        url: "https://pconjsr.in/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fhero-2.342b8b55.jpeg&w=3840&q=75",
+        width: 1200,
+        height: 630,
+        alt: "PCON: Programming Club Of NIT Jamshedpur",
+      },
+    ],
+  },
+  icons: {
+    shortcut: "/favicon.ico",
+  },
 };
 
 export default async function RootLayout({
@@ -31,6 +56,28 @@ export default async function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Sometype+Mono:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap"
           rel="stylesheet"
         />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </head>
       <body>
         <Providers>
